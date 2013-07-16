c645cfa6de0e2725114b5f9b1cfefda9f5b24cce
Willyham <willdemaine@gmail.com>
2013-07-15 23:52:31 -0700

    Draw play with ID when create returns

--- app/scripts/Views/PlaybookPlayView.js
+++ app/scripts/Views/PlaybookPlayView.js
@@ -6,17 +6,19 @@ define([
 
             tagName:'li',
             template: _.template('<a href="/plays/<%= id %>"><%= name %></a><em class="delete">[X]</em>'),
-
             model: null,
+
+            initialize: function(){
+                this.listenTo(this.model, 'change', this.render);
+            },
 
             events: {
                 'click .delete': 'removePlay'
             },
 
             render: function(){
-                var id = this.model.isNew() ? this.model.get('cId') : this.model.get('id');
                 this.$el.html(this.template({
-                    id: id,
+                    id: this.model.get('id'),
                     name: this.model.get('name')
                 }));
                 return this.$el;
