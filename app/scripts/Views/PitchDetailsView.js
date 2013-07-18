410bf1770dcf4b5a42d7a057b10f62cb9c5fea13
Willyham <willdemaine@gmail.com>
2013-07-17 21:37:39 -0700

    Added ability to rename play

--- app/scripts/Views/PitchDetailsView.js
+++ app/scripts/Views/PitchDetailsView.js
@@ -4,19 +4,26 @@ define([
     'backbone'], function($,_,Backbone){
         var PitchDetailsView = Backbone.View.extend({
 
-            template: _.template('<h2><%= name %></h2>'),
+            className: 'pitchDetails',
+            template: _.template('<h2><input type="text" value="<%= name %>"/></h2>'),
             _model: null,
 
             initialize: function(model){
-                this._model = model
+                this._model = model;
+                this.listenTo(this._model, 'change:name', this.render);
             },
 
-            listenToModel: function(){
-                this.listenTo(this._model, 'change:name', this.render);
+            events: {
+                'change input': 'renamePlay'
             },
 
             render: function(){
                 return this.$el.html(this.template(this._model.toJSON()));
+            },
+
+            renamePlay: function(event){
+                this._model.set('name', event.target.value);
+                this._model.save();
             }
         });
         return PitchDetailsView;
