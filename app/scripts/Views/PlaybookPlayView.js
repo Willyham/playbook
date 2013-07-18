e30e7a7035e212eb13547926ec7dcd718c49e170
Willyham <willdemaine@gmail.com>
2013-07-17 22:24:21 -0700

    Move play icons to support multiple types

--- app/scripts/Views/PlaybookPlayView.js
+++ app/scripts/Views/PlaybookPlayView.js
@@ -5,7 +5,7 @@ define([
         var PlaybookPlayView = Backbone.View.extend({
 
             tagName:'li',
-            template: _.template('<div class="playItem <%= selected%>"><img class="playTypeIcon" src="../../images/football.svg"/><a href="#/play/<%= id %>"><%= name %></a><span class="playActions"><em class="playAction copy" data-id="<%= id %>">copy</em><em class="playAction delete">delete</em></span></div>'),
+            template: _.template('<div class="playItem <%= selected%>"><img class="playTypeIcon" src="../../images/playTypes/<%=type%>.svg"/><a href="#/play/<%= id %>"><%= name %></a><span class="playActions"><em class="playAction copy" data-id="<%= id %>">copy</em><em class="playAction delete">delete</em></span></div>'),
             model: null,
 
             initialize: function(){
@@ -21,6 +21,7 @@ define([
                 this.$el.html(this.template({
                     id: this.model.get('id'),
                     name: this.model.get('name'),
+                    type: this.model.get('type'),
                     selected: this.model.get('selected') ? 'selected' : ''
                 }));
                 return this.$el;
